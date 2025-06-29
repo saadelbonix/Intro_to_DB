48a33375ef1be85d2d036fac6f29bfa66699dda8
saadelbonix <saad.elboni2@gmail.com>
2025-06-29 23:53:23 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,2 @@
--- Insert a single row into the CUSTOMERS table
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
