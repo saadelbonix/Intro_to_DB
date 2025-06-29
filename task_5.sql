5c6741c913919f0c133be9c355183f15a9664e65
saadelbonix <saad.elboni2@gmail.com>
2025-06-29 23:53:56 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,2 @@
-echo "INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');" > task_5.sql
-
-git add task_5.sql
-git commit -m "Add script to insert row into customers table"
-git push origin main
+INSERT INTO CUSTOMERS (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
